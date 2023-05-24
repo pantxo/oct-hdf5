bbc8d9aaf8fe0a8a7278bd299960153261ffc51a
Pantxo Diribarne <pantxo.diribarne@gmail.com>
2023-05-24 21:12:26 +0200

    h5readatt: add documentation

--- src/h5readatt.m
+++ src/h5readatt.m
@@ -14,52 +14,60 @@
 ## along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {} {@var{val} =} h5readatt (@var{fname}, @var{path}, @var{attr})
+## @deftypefn {} {@var{val} =} h5readatt (@var{fname}, @var{loc}, @var{attr})
+## Read data associated to a group or dataset attribute @var{attr}, at location
+## @var{loc}.
 ##
-## @seealso{}
+## The location @var{loc} must be a string indicating the path to the
+## related group or dataset.
+## @example
+## ## Read attribute "color" in dataset "triangle" located at the root of an hdf5 file
+## color = h5readatt ("some_file.h5", "/triangle", "color");
+## @end example
+## @seealso{h5info}
 ## @end deftypefn
 
 function data = h5readatt (fname, path, attr)
-  
+
   file = H5F.open (fname);
-  
+
   infobuf = H5O.get_info_by_name (file, path, "H5P_DEFAULT");
 
   obj_id = [];
   attr_id = [];
-  
+
   unwind_protect
-    
-    
+
+
     switch (infobuf.type)
-           
+
       case H5ML.get_constant_value ("H5O_TYPE_GROUP")
         obj_id = H5G.open (file, path, "H5P_DEFAULT");
         attr_id = H5A.open (obj_id, attr, "H5P_DEFAULT");
         data = H5A.read (attr_id);
-        
+
       case H5ML.get_constant_value ("H5O_TYPE_DATASET")
         obj_id = H5D.open (file, path, "H5P_DEFAULT");
         attr_id = H5A.open (obj_id, attr, "H5P_DEFAULT");
         data = H5A.read (attr_id);
-        
+
       otherwise
         error ("h5readatt: unhandled object type")
-        
+
     endswitch
-    
+
   unwind_protect_cleanup
-    
+
     if (! isempty (attr_id))
       H5A.close (attr_id);
     endif
-    
+
     if (! isempty (obj_id))
       H5O.close (obj_id);
     endif
-    
+
     H5F.close (file);
-    
+
   end_unwind_protect
-  
+
 endfunction
