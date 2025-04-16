f81f4a0a3b6036b5732b7e09be6cbfa7ec9acd6d
Pantxo Diribarne <pantxo.diribarne@gmail.com>
2025-04-16 23:21:57 +0200

    write_mat73.m: try again add to index

--- src/write_mat73.m
+++ src/write_mat73.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2023 Pantxo DIRIBARNE PD218333
+## Copyright (C) 2023-2025 Pantxo DIRIBARNE PD218333
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
